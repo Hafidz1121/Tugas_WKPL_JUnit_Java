f2922cad0e8a29bcd01030b58a37aaa18d0b1533
Hafidz Fadhillah Febrianto <63035660+Hafidz1121@users.noreply.github.com>
2023-10-31 07:47:24 +0700

    Add Comment Detail Information Source Code

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -3,11 +3,17 @@ package org.example;
 import java.util.stream.DoubleStream;
 
 public class Calculator {
+    // method static dengan parameter double dan menggunakan varargs (variadic arguments)
     static double add(double... operands) {
+        // mengembalikan nilai dengan mengambil array dalam parameter
+        // dan mengonversi menjadi aliran (stream) serta menghitung jumlah semua angka dalam aliran tersebut
         return DoubleStream.of(operands).sum();
     }
 
+    // method static dengan parameter double dan menggunakan varargs (variadic arguments)
     static double multiply(double... operands) {
+        // mengembalikan nilai dengan mengambil array dalam parameter
+        // dan mengkonversi menjadi aliran (stream) serta menghitung hasil perkalian semua angka dalam aliran tersebut
         return DoubleStream.of(operands).reduce(1, (a, b) -> a * b);
     }
 }
--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -8,11 +8,14 @@ class CalculatorTest {
 
     @Test
     void add() {
+        // method untuk memvalidasi bahwa nilai aktual dan nilai yang diharapkan adalah sama
         assertEquals(6, Calculator.add(2, 4));
     }
 
     @Test
     void multiply() {
+        // method untuk memvalidasi seluruh assertion dan menampilkan pesan sukses / gagal
+        // jika terdapat gagal maka terdapat pesan kesahalan berserta jawaban yang benar
         assertAll(() -> assertEquals(4, Calculator.multiply(2, 2)),
                 () -> assertEquals(-4, Calculator.multiply(2, -2)),
                 () -> assertEquals(4, Calculator.multiply(-2, -2)),
