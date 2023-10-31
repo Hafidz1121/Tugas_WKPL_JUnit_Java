88a6b27b9878cf21f2fcb33782da32f414e01ae9
Hafidz Fadhillah Febrianto <63035660+Hafidz1121@users.noreply.github.com>
2023-10-31 07:40:23 +0700

    Add Source Code And Testing Unit

---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void add() {
+        assertEquals(6, Calculator.add(2, 4));
+    }
+
+    @Test
+    void multiply() {
+        assertAll(() -> assertEquals(4, Calculator.multiply(2, 2)),
+                () -> assertEquals(-4, Calculator.multiply(2, -2)),
+                () -> assertEquals(4, Calculator.multiply(-2, -2)),
+                () -> assertEquals(0, Calculator.multiply(1, 0)));
+    }
+}
